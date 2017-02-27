3b0eca81e11637d82f7af38b79170ad74aeaa88b
Johnny Wu <KarasAya@gmail.com>
2017-02-27 11:09:31 +0800

    simple refine

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict';
 
 const platform = require('electron-platform');
+const pkgJson = require('./package.json');
 
 let panel;
+let name = `__electron_panel__`;
+let msg = `Failed to require ${pkgJson.name}@${pkgJson.version}:
+  A different version of ${pkgJson.name} already running in the process, we will redirect to it.
+  Please make sure your dependencies use the same version of ${pkgJson.name}.`;
 
 if ( platform.isMainProcess ) {
-  panel = require('./lib/main');
+  if (global[name]) {
+    console.warn(msg);
+    panel = global[name];
+  } else {
+    panel = global[name] = require('./lib/main');
+  }
 } else {
-  panel = require('./lib/renderer/index');
+  if (window[name]) {
+    console.warn(msg);
+    panel = window[name];
+  } else {
+    panel = window[name] = require('./lib/renderer/index');
+  }
 }
 
 // ==========================
